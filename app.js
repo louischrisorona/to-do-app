360c6a70ccf036fe98324e05c9fbfc2493a10227
Chris <louischrisorona@gmail.com>
2019-11-13 18:38:39 -0800

    Included vanilla JS to add elements and attributes for a new Todo item

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-let todos = [];
-
-const getItem = (todo) => {
-    //code to retrieve an item from localStorage API
-    let item = localStorage.getItem('todo')
-    console.log(`${todo} was found.`)
-    return item
-}
+let todos = []
+const tbl = document.querySelector('.todo-table')
 
 const saveItem = (todo) => {
-    //code to set an item to local storage
-    todos.push(todo);
-    localStorage.setItem(todos.length, 'todo')
-    alert(`${todo} was added.`)
+    todos.push(todo)
 }
 
+let btn = document.querySelector('#add-btn')
+btn.addEventListener('click', (event) => {
+    event.preventDefault()
+    let todoItem = document.querySelector('#todo-value').value
+    saveItem(todoItem)
+    updateList(todoItem)
+})
+
+const updateList = (todo) => {
+    //for every new item make a new row
+    let newRow = document.createElement('TR')
+    //for every new row make a new data cell
+    let newEl = document.createElement('TD')
+    let newIn = document.createElement('INPUT')
+    let newAtt = document.createAttribute('type')
+    let newDel = document.createElement('BUTTON')
+    let delAtt = document.createAttribute('type')
+    newDel.innerText = "X"
+    delAtt.value = "button"
+    newAtt.value = "checkbox"
+    newIn.setAttributeNode(newAtt)
+    tbl.appendChild(newRow).appendChild(newEl).appendChild(newIn)
+    newEl.insertAdjacentText('beforeend', todo)
+    newEl.insertAdjacentElement('afterend', newDel)
+}
